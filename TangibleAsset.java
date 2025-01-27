2254202b026a5a1e2eb28d76764b1b846b401a70
topia-cloud <naturalhorn@hotmail.com>
2025-01-27 13:52:53 +0900

    Create TangibleAsset.java

---
+++ TangibleAsset.java
@@ -0,0 +1,14 @@
+public abstract class TangibleAsset{
+  private String name;
+  private int price;
+  private String colour;
+  public TangibleAsset(String name, int price, String colour){
+    this.name = name;
+    this.price = price;
+    this colour = colour;
+  }
+  public String getName(){return this.name;}
+  public int getPrice(){return this.price;)}
+  public String getColour(retrun this.colour;)}
+}
+  
